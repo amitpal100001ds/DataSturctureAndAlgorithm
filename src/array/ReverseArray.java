8b019a65c0edcef707f47e12103471bae92fcc50
Nigam <nigam.gbusloutions123@gmail.com>
2021-01-14 11:06:34 +0530

    Remove Main Program class

--- src/array/ReverseArray.java
+++ src/array/ReverseArray.java
@@ -1,21 +1,29 @@
 package array;
 
 public class ReverseArray {
-	public void reverseArray(int arr[],int start, int end) {
+	public void reverseArray(int arr[], int start, int end) {
 
 		while (start < end) {
-			swap(arr,start, end);
+			swap(arr, start, end);
 			start++;
 			end--;
 		}
-		for(int i=0;i<arr.length;i++) {
-			System.out.print(" "+arr[i]);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(" " + arr[i]);
 		}
 	}
 
-	public void swap(int arr[],int start, int end) {
+	public void swap(int arr[], int start, int end) {
 		int temp = arr[start];
 		arr[start] = arr[end];
 		arr[end] = temp;
+	}
+
+	public static void main(String arg[]) {
+		int arr[] = { 2, 5, 2, 80, 6, 7, 6 };
+
+		// Reverse array program
+		ReverseArray reverseArray = new ReverseArray();
+		reverseArray.reverseArray(arr, 0, arr.length - 1);
 	}
 }
